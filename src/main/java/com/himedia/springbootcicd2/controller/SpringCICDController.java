a417db428336020319476c943fb2dfde50886313
yunhyunglee <yunhyung91@gmail.com>
2025-03-05 12:01:40 +0900

    action start

--- src/main/java/com/himedia/springbootcicd2/controller/SpringCICDController.java
+++ src/main/java/com/himedia/springbootcicd2/controller/SpringCICDController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class SpringCICDController {
     @GetMapping("/")
     public String index() {
-        return "Hello World";
+        return "Hello World Hello World Hello World Hello World Hello World ";
     }
 }
